7940cf7fb9cbd55e9242eb139e2ff20b648a34d9
pjs18-spec <pauljustinsharma18@gmail.com>
2026-01-25 12:23:02 -0700

    Remove BiologicalSex enum implementation

--- HabitPeakAI/BiologicalSex.swift
+++ HabitPeakAI/BiologicalSex.swift
@@ -1,13 +1,1 @@
-import Foundation
-
-enum BiologicalSex: String, CaseIterable, Codable, Identifiable {
-    case male = "Male"
-    case female = "Female"
-    case other = "Other"
-    
-    var id: String { rawValue }
-    
-    var displayName: String {
-        rawValue
-    }
-}
+i
